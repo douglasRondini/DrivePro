5663c175c749328b10b842a6736fae5d712b2703
douglas Rondini <douglasfeliperondine@gmail.com>
2026-08-03 17:21:56 -0300

    refactor: otimizar navegação inferior e estabilidade das Activities
    - Atualizada recuperação do NavController para usar supportFragmentManager (melhor prática).\n- Adicionado OnItemReselectedListener para evitar recarregamento desnecessário de abas.\n- Desabilitado enableEdgeToEdge nas Home Activities para melhor suporte ao adjustResize.\n- Sincronizados IDs de menu e gráfico de navegação para prevenir falhas de destino.

--- app/src/main/java/com/douglasrondini/drive_20_android/ui/activities/BottomNavHomeActivity.kt
+++ app/src/main/java/com/douglasrondini/drive_20_android/ui/activities/BottomNavHomeActivity.kt
@@ -1,11 +1,8 @@
 package com.douglasrondini.drive_20_android.ui.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.douglasrondini.drive_20_android.R
 import com.douglasrondini.drive_20_android.databinding.ActivityBottomNavHomeBinding
@@ -15,14 +12,15 @@ class BottomNavHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityBottomNavHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment_HomeAluno)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_HomeAluno) as NavHostFragment
+        val navController = navHostFragment.navController
 
         binding.bottomNavigation.setupWithNavController(navController)
-
-
+        
+        // Evita recarregar a mesma tela se clicar no item já selecionado
+        binding.bottomNavigation.setOnItemReselectedListener { /* do nothing */ }
     }
 }
